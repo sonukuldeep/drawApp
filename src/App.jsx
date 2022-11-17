a28b4f387ae46a48c1f5d6f1aaf4e51068947540
sonukuldeep <sonukuldeep@outlook.com>
2022-11-18 01:20:52 +0530

    fixed some mysterious bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import dist from '@vitejs/plugin-react'
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  document.addEventListener('touchstart', e=>{
-    [...e.changedTouches].forEach(touch=>{
+  document.addEventListener('touchstart', e => {
+    [...e.changedTouches].forEach(touch => {
       const dot = document.createElement('div')
       dot.classList.add('dot')
       dot.style.top = `${touch.pageY}px`
@@ -14,14 +12,17 @@ function App() {
     })
   })
 
-  function remove(){
+  function remove() {
     const dots = document.querySelectorAll('.dot')
-    dots.forEach(dot=>{dot.remove()})
+    dots.forEach(dot => { dot.remove() })
   }
   return (
     <>
+
       <div className='topHalf'></div>
-      <button onClick={remove}>Clear</button>
+      <div className='relative'>
+        <button onClick={remove} className="p-2 border-2 absolute left-[50%] translate-x-[-50%] top-2 border-[#333] hover:border-[#fff] rounded-md bg-white hover:bg-[#333] text-[#333] hover:text-white">Clear</button>
+      </div>
     </>
   )
 }
