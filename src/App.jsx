36d953f4e2e747e491cd10ff2168be70a0c72961
sonukuldeep <sonukuldeep@outlook.com>
2022-11-18 00:47:15 +0530

    bug fix and remove btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import dist from '@vitejs/plugin-react'
 import { useState } from 'react'
 import './App.css'
 
@@ -6,15 +7,21 @@ function App() {
     [...e.changedTouches].forEach(touch=>{
       const dot = document.createElement('div')
       dot.classList.add('dot')
-      dot.style.top = `${touch.pageX}px`
-      dot.style.left = `${touch.pageY}px`
+      dot.style.top = `${touch.pageY}px`
+      dot.style.left = `${touch.pageX}px`
       dot.id = touch.identifier
       document.body.append(dot)
     })
   })
+
+  function remove(){
+    const dots = document.querySelectorAll('.dot')
+    dots.forEach(dot=>{dot.remove()})
+  }
   return (
     <>
       <div className='topHalf'></div>
+      <button onClick={remove}>Clear</button>
     </>
   )
 }
